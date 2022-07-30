8158edb7fcc19e06067c80cc033e8ba3b581d82e
Rizky Adi Prasetyo <rizky@jayantara.net>
2022-07-30 13:37:28 +0700

    First Commit

---
+++ MonitorKontrolTambakUdangVaname.ino
@@ -0,0 +1,86 @@
+#include <WiFi.h>
+
+#include "ThingsBoard.h"
+#include "DHTesp.h"
+
+#define CURRENT_FIRMWARE_TITLE    "TEST"
+#define CURRENT_FIRMWARE_VERSION  "1.0.0"
+
+#define WIFI_SSID           "Wokwi-GUEST"
+#define WIFI_PASSWORD       ""
+
+// See https://thingsboard.io/docs/getting-started-guides/helloworld/
+// to understand how to obtain an access token
+#define TOKEN               "2VacTghl150ZqZojc6Pp"
+#define THINGSBOARD_SERVER  "thingsboard.cloud"
+
+const int DHT_PIN = 15;
+
+DHTesp dhtSensor;
+
+WiFiClient espClient;
+ThingsBoard tb(espClient);
+int status = WL_IDLE_STATUS;
+
+void InitWiFi()
+{
+  Serial.println("Sedang Mengkoneksikan ke Akses Point ...");
+  // attempt to connect to WiFi network
+
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  while (WiFi.status() != WL_CONNECTED) {
+
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("Terhubung ke Akses Poin");
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  status = WiFi.status();
+  if ( status != WL_CONNECTED) {
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
+    }
+  Serial.println("Terhubung ke Akses Poin");
+  }
+}
+
+void setup() {
+  // initialize serial for debugging
+  Serial.begin(115200);
+  Serial.println();
+  InitWiFi();
+  dhtSensor.setup(DHT_PIN, DHTesp::DHT22);
+}
+
+void loop() {
+  delay(1000);
+
+  if (WiFi.status() != WL_CONNECTED) {
+    reconnect();
+  }
+
+  if (!tb.connected()) {
+    // Connect to the ThingsBoard
+    Serial.print("Koneksi ke: ");
+    Serial.print(THINGSBOARD_SERVER);
+    Serial.print(" dengan token ");
+    Serial.println(TOKEN);
+    if (!tb.connect(THINGSBOARD_SERVER, TOKEN)) {
+      Serial.println("Gagal untuk mengkoneksikan");
+      return;
+    }
+  }
+  Serial.println("Mengirim data...");
+
+  TempAndHumidity  data = dhtSensor.getTempAndHumidity();
+  tb.sendTelemetryInt("temperature", data.temperature);
+  Serial.print("Suhu : ");
+  Serial.print(data.temperature);
+
+  tb.loop();
+}
